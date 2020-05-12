fa500001d1e90e7915c61d3f4f0f457a38b30c31
Nikola Babic <babicnikola1@gmail.com>
2020-05-12 19:37:06 +0200

    Igraci frontend bez funckionalnih filtera

--- Faza 5/portal za sahovski savez/resources/views/master.blade.php
+++ Faza 5/portal za sahovski savez/resources/views/master.blade.php
@@ -23,6 +23,9 @@
                      <li class="nav-item">
                        <a class="nav-link text-dark" href="/turnir"> Turniri</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="/igrac"> Igraci</a>
+                     </li>
                 </ul>
             </div>
 
--- Faza 5/portal za sahovski savez/resources/views/players.blade.php
+++ Faza 5/portal za sahovski savez/resources/views/players.blade.php
@@ -3,9 +3,101 @@
 @section('content')
 
     <h1>Igraci</h1>
+    <div class="container">
+        <div class="row">
+          <div class="col-sm-3">
+            <p>Filter</p>
+            <div class="card">
+              <article class="card-group-item">
+                <header class="card-header">
+                  <h6 class="title">Ime </h6>
+                </header>
+                <div class="filter-content">
+                  <div class="card-body">
+                    <input type="text" class="form-control">
 
-    @foreach($players as $player)
-    <p> {{ $player->name }}</p> <br>
-    @endforeach
-    <a href="{{action('HomeController@index')}}"> Nazad na pocetnu</a>
+                  </div> <!-- card-body.// -->
+                </div>
+              </article> <!-- card-group-item.// -->
+              <article class="card-group-item">
+                <header class="card-header">
+                  <h6 class="title">Pol </h6>
+                </header>
+                <div class="filter-content">
+                  <div class="card-body">
+                    <label class="form-check">
+                      <input class="form-check-input" type="radio" name="exampleRadio" value="">
+                      <span class="form-check-label">
+                        Musko
+                      </span>
+                    </label>
+                    <label class="form-check">
+                      <input class="form-check-input" type="radio" name="exampleRadio" value="">
+                      <span class="form-check-label">
+                        Zensko
+                      </span>
+                    </label>
+                  </div> <!-- card-body.// -->
+                </div>
+              </article> <!-- card-group-item.// -->
+
+              <article class="card-group-item">
+                <header class="card-header">
+                  <h6 class="title">Rejting</h6>
+                </header>
+                <div class="filter-content">
+                  <div class="card-body">
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label>Min</label>
+                        <input type="number" class="form-control" id="inputEmail4" placeholder="0">
+                      </div>
+                      <div class="form-group col-md-6 text-right">
+                        <label>Max</label>
+                        <input type="number" class="form-control" placeholder="3000">
+                      </div>
+                    </div>
+                  </div> <!-- card-body.// -->
+                </div>
+              </article> <!-- card-group-item.// -->
+            </div>
+          </div>
+
+
+
+          <table class="table table-hover m-auto col-sm-9">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Ime</th>
+                <th scope="col">Prezime</th>
+                <th scope="col">Rejting</th>
+                <th scope="col" class="sudija">Vise</th>
+              </tr>
+            </thead>
+            <tbody>
+                        
+            @php
+            $i = 1
+            @endphp
+
+            @foreach($players as $player)
+            <tr>
+                <td>{{ $i }}</td>                
+                <td>{{ $player->name }}</td>
+                <td>{{ $player->surname }}</td>
+                <td>{{ $player->rating }}</td>
+                <td><a class="btn btn-primary" href="igrac/{{ $player->id }}">+</a></td>
+                @php
+                $i++;
+                @endphp
+            </tr>
+            @endforeach
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
 @endsection
